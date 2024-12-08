475b3ad25fc17e861f2c99f5262c707646904228
Kenneth Emanuel Cabusao <119332036+kentmnlcbso@users.noreply.github.com>
2024-12-08 14:06:05 +0000

    Done adding output

--- PrimitiveConcatenation.java
+++ PrimitiveConcatenation.java
@@ -1,1 +1,14 @@
+public class PrimitiveConcatenation {
+    public static void main(String[] args) {
+        byte b = 72;
+        short s = 3;
+        int i = 1;
+        char c = 'w';
+        float f = 2.0f;
+        boolean bool = true;
 
+        String output = (char)(b) + "" + s + "11" + "0 " + c + "0r" + i + "d " + f + " " + bool;
+
+        System.out.println(output);
+    }
+}
